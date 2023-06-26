a152eab66a468afda3fdd920d0d7be7f53148b07
Srijan Verma <srijanverma102@gmail.com>
2023-06-26 22:04:45 +0530

    initial dev

---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+import { Box, Button, HStack, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react'
+import { AiOutlineSend, AiFillLinkedin, AiFillGithub, AiFillTwitterCircle} from "react-icons/ai"
+
+const Footer = () => {
+  return (
+    <Box bgColor={"blackAlpha.900"} minH={"40"} p={"16"} color={"white"}>
+
+        <Stack direction={['column','row']}>
+
+            <VStack alignItems={"stretch"} w={"full"} p={"4"} color={"white"}>
+                <Heading size={"md"} textTransform={"uppercase"} textAlign={['center', 'left']}>
+                    Subcribe to get updates
+                </Heading>
+
+                <HStack borderBottom={'2px solid white'} py="2">
+                    <Input placeholder="Enter Email here..." 
+                        borderRadius={'none'}
+                        border={'none'} outline={"none"} focusBorderColor="none"
+                    />
+
+                    <Button p={"0"} colorScheme='purple' variant={"ghost"} borderRadius={"0 20px 20px 0"} >
+                        <AiOutlineSend size={"20"}/>
+                    </Button>
+                </HStack>
+            </VStack>
+
+            <VStack w={'full'} borderLeft={['none', '1px solid white']} borderRight={['none','1px solid white']}>
+                <Heading textTransform={"uppercase"} textAlign={"center"}>
+                    VIDEO HUB
+                </Heading>
+                <Text>All rights reserved</Text>
+            </VStack>
+
+            <VStack w={'full'}>
+                <Heading size={"md"} textTransform={"uppercase"}>
+                    Social Media
+                </Heading>
+
+                <Button variant={"ghost"} colorScheme='whiteAlpha'>
+                    <a target={'blank'} href="https://www.linkedin.com/in/srijan-verma-0407sv">
+                        <AiFillLinkedin fontSize={"2rem"} color='white'/>
+                    </a> 
+                </Button>
+
+                <Button variant={"ghost"} colorScheme='whiteAlpha'>
+                    <a target={'blank'} href="https://twitter.com/Srijan__verma">
+                        <AiFillTwitterCircle fontSize={"2rem"} color='white'/>
+                    </a> 
+                </Button>
+
+                <Button variant={"ghost"} colorScheme='whiteAlpha'>
+                    <a target={'blank'} href="https://github.com/SrijanVerma04">
+                        <AiFillGithub fontSize={"2rem"} color='white'/>
+                    </a>
+                </Button>
+            </VStack>
+
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer
